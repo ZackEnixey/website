51fdff1a0169b04256a76dd5715a6ade5c0fe498
Vidosava Vidic Vida <zzftn90@gmail.com>
2019-10-05 17:00:50 +0200

    Updates

---
+++ precache-manifest.9587c147477fda989f8db276fa200ac2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f311a2bb289d129a8f5bf5c4a10ba6b8",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "3bcd88acee988467c6ec",
+    "url": "/website/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "404418bcf95f30729e57",
+    "url": "/website/static/js/2.2379b304.chunk.js"
+  },
+  {
+    "revision": "3bcd88acee988467c6ec",
+    "url": "/website/static/js/main.6a6638c8.chunk.js"
+  },
+  {
+    "revision": "ffa6f98461958ed00955",
+    "url": "/website/static/js/runtime-main.09708970.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/website/static/media/logo.25bf045c.svg"
+  }
+]);
